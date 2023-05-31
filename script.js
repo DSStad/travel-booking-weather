e727eaa9a2b560c99a398ce959bdc630f80aa388
unknown <john.gordos@outlook.com>
2023-05-31 11:00:31 -0600

    proof of work so far

--- script.js
+++ script.js
@@ -17,22 +17,11 @@ function handleRouteMe(event) {
   event.preventDefault();
   var originCity = $("#origin-city").val();
   var originState = $("#origin-state").val();
-
-  var originCoords = retrieveGeoCoordinates(originCity, originState);
-  //var route = retrieveRoute(originCoords, finalCoords);
-  // if (
-  //   originCoords.lat != "" &&
-  //   originCoords.lon != "" &&
-  //   finalCoords.lat != "" &&
-  //   finalCoords.lon != ""
-  // ) {
-
-  // } else {
-  //   alert("GEO CODING ERROR!  USE YOUR IMAGINATION!");
-  // }
+  //RENAME
+  retrieveGeoCoordinates(null, originCity, originState);
 }
-
-function retrieveGeoCoordinates(city, state) {
+//research async await
+function retrieveGeoCoordinates(originCoords, city, state) {
   fetch(
     "http://api.openweathermap.org/geo/1.0/direct?q=" +
       city +
@@ -75,63 +64,14 @@ function retrieveGeoCoordinates(city, state) {
         );
       }
       console.log(coordinateResults);
-      if (coordinateResults.lat) {
-        //MAKE THIS BETTER!
-        var finalCity = $("#final-city").val();
-        var finalState = $("#final-state").val();
-        retrieveFinalGeoCoordinates(coordinateResults, finalCity, finalState);
-      }
-      //return coordinateResults;
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-}
-//make one function, check first arg to determine if retrieveRoute should be called
-function retrieveFinalGeoCoordinates(originCoords, city, state) {
-  fetch(
-    "http://api.openweathermap.org/geo/1.0/direct?q=" +
-      city +
-      "," +
-      state +
-      ",US&limit=10&appid=5a5f2543215b0ae09a5dc07887c20551"
-  )
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data);
-      var coordinateResults = new Coordinates();
-      if (data.length > 1) {
-        //Do we want the user to pick the relevant option if multiple results are returned?
-        for (var i = 0; i < data.length; i++) {
-          if (
-            data[i].state == state &&
-            !Number.isNaN(data[i].lat) &&
-            !Number.isNaN(data[i].lon)
-          ) {
-            coordinateResults.lat = data[i].lat;
-            coordinateResults.lon = data[i].lon;
-          }
+      if (coordinateResults.lat && coordinateResults.lon) {
+        if (!originCoords) {
+          var finalCity = $("#final-city").val();
+          var finalState = $("#final-state").val();
+          retrieveGeoCoordinates(originCoords, finalCity, finalState);
+        } else {
+          retrieveRoute(originCoords, coordinateResults);
         }
-      } else if (
-        data.length == 1 &&
-        !Number.isNaN(data[0].lat) &&
-        !Number.isNaN(data[0].lon)
-      ) {
-        coordinateResults.lat = data[0].lat;
-        coordinateResults.lon = data[0].lon;
-      } else {
-        alert(
-          "FINAL GRAB NO CITY STATE FOUND BASED ON WHAT YOU ENTERED. CITY: " +
-            city +
-            " and STATE: " +
-            state
-        );
-      }
-      if (coordinateResults.lat) {
-        //originCoords coordinateResults
-        retrieveRoute(originCoords, coordinateResults);
       }
       //return coordinateResults;
     })
@@ -143,11 +83,11 @@ function retrieveRoute(originCoords, finalCoords) {
   var genericCoords =
     originCoords.lat +
     "," +
-    originCoords.long +
+    originCoords.lon +
     ":" +
     finalCoords.lat +
     "," +
-    finalCoords.long;
+    finalCoords.lon;
   fetch(
     "https://api.tomtom.com/routing/1/calculateRoute/" +
       genericCoords +
@@ -161,11 +101,11 @@ function retrieveRoute(originCoords, finalCoords) {
       console.log(data);
       console.log(data.routes.length);
 
-      for (var i = 0; i < data.routes.length; i++) {
+      for (var j = 0; j < data.routes.length; j++) {
         console.log("leg " + i);
         console.log(data.routes[i].legs);
         console.log(data.routes[i].legs.length);
-        for (var j = 0; j < data.routes[i].legs.length; i++) {
+        for (var k = 0; k < data.routes[j].legs.length; k++) {
           console.log("point " + i);
           console.log(data.routes[i].legs.points);
         }
